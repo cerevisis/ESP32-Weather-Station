94ca85734c247cae173a09dc800a3b6c90552dc4
cerevisis <66214741+cerevisis@users.noreply.github.com>
2024-05-23 20:42:48 +0200

    Update update.js
    bug fixes and optimisations

--- data/update.js
+++ data/update.js
@@ -53,18 +53,31 @@ function onClose(event) {
 	setTimeout(initWebSocket, 1000);
 }
 
+// let userSelection = document.getElementsByClassName("form-check-input");
+//let userSelection = document.getElementsByTagName('input');
+
+let allCheckboxes = document.querySelectorAll('input[type=checkbox]');
+console.log("Checkbox Array size:" + allCheckboxes.length);
+
+for (let i = 0; i < allCheckboxes.length; i++) {
+	console.log(allCheckboxes[i]);
+	console.log(document.getElementById(allCheckboxes[i].id));
+	allCheckboxes[i].addEventListener("change", updateToggle)
+
+}
 
 function updateToggle(element) {
-	var idLength = element.id.length;
-	var togType = element.id.charAt(0);
+	console.log(element)
+	var idLength = element.target.id.length;
+	var togType = element.target.id.charAt(0);
 	console.log("togType: " + togType + " idLength: " + idLength);
 	if (idLength == 8) {
-		var toggNumber = element.id.substr(idLength - 1, 1);
+		var toggNumber = element.target.id.substr(idLength - 1, 1);
 	}
 	else if (idLength == 9) {
-		var toggNumber = element.id.substr(idLength - 1, 2);
+		var toggNumber = element.target.id.substr(idLength - 1, 2);
 	}
-	var isChecked = document.getElementById(element.id).checked;
+	var isChecked = document.getElementById(element.target.id).checked;
 	console.log("toggle: " + toggNumber + " " + isChecked);
 
 	document.getElementById(togType + "toggle" + toggNumber).innerHTML = isChecked;
@@ -78,18 +91,26 @@ function updateToggle(element) {
 	}
 }
 
+let allTimeInputs = document.querySelectorAll('input[type=time]');
+console.log("Time Array size:" + allTimeInputs.length);
 
+for (let i = 0; i < allTimeInputs.length; i++) {
+	console.log(allTimeInputs[i]);
+	console.log(document.getElementById(allTimeInputs[i].id));
+	allTimeInputs[i].addEventListener("change", updateTimeInput)
+
+}
 
 function updateTimeInput(element) {
-	var idLength = element.id.length;
-	var timeType = element.id.charAt(0);
-	var time = document.getElementById(element.id).value;
+	var idLength = element.target.id.length;
+	var timeType = element.target.id.charAt(0);
+	var time = document.getElementById(element.target.id).value;
 	console.log("timeType: " + timeType + " idLength: " + idLength);
 	if (idLength == 6) {
-		var timeNumber = element.id.substr(idLength - 1, 1);
+		var timeNumber = element.target.id.substr(idLength - 1, 1);
 	}
 	else if (idLength == 7) {
-		var timeNumber = element.id.substr(idLength - 1, 2);
+		var timeNumber = element.target.id.substr(idLength - 1, 2);
 	}
 
 	websocket.send(timeNumber + timeType + time);
@@ -97,20 +118,30 @@ function updateTimeInput(element) {
 
 }
 
+let allNumberInputs = document.querySelectorAll('input[type=number]');
+console.log("Time Array size:" + allNumberInputs.length);
+
+for (let i = 0; i < allNumberInputs.length; i++) {
+	console.log(allNumberInputs[i]);
+	console.log(document.getElementById(allNumberInputs[i].id));
+	allNumberInputs[i].addEventListener("change", sendForm)
+
+}
+
 function sendForm(element) {
 
-	var idLength = element.id.length;
-	var formType = element.id.charAt(0);
-	var value = document.getElementById(element.id).value;
+	var idLength = element.target.id.length;
+	var formType = element.target.id.charAt(0);
+	var value = document.getElementById(element.target.id).value;
 	console.log("formType: " + formType + " idLength: " + idLength);
 	if (idLength == 6) {
-		var formNumber = element.id.substr(idLength - 1, 1);
+		var formNumber = element.target.id.substr(idLength - 1, 1);
 	}
 	else if (idLength == 7) {
-		var formNumber = element.id.substr(idLength - 1, 2);
+		var formNumber = element.target.id.substr(idLength - 1, 2);
 	}
 	else if (idLength == 8) {
-		var formNumber = element.id.substr(idLength - 1, 3);
+		var formNumber = element.target.id.substr(idLength - 1, 3);
 	}
 
 	websocket.send(formNumber + formType + value);
@@ -119,19 +150,29 @@ function sendForm(element) {
 
 }
 
+let allDropDownInputs = document.querySelectorAll('input[type=submit]');
+console.log("Time Array size:" + allDropDownInputs.length);
+
+for (let i = 0; i < allDropDownInputs.length; i++) {
+	console.log(allDropDownInputs[i]);
+	console.log(document.getElementById(allDropDownInputs[i].id));
+	allDropDownInputs[i].addEventListener("change", getDropdown)
+
+}
+
 function getDropdown(element) {
-	var dropDown = document.getElementById(element.id);
+	var dropDown = document.getElementById(element.target.id);
 	var selectedValue = dropDown.options[dropDown.selectedIndex].text;
 	console.log("Selected Text: " + dropDown + " Value: " + selectedValue);
 
-	var idLength = element.id.length;
-	var dropType = element.id.charAt(0);
+	var idLength = element.target.id.length;
+	var dropType = element.target.id.charAt(0);
 	console.log("dropType: " + dropType + " idLength: " + idLength);
 	if (idLength == 8) {
-		var dropNumber = element.id.substr(idLength - 1, 1);
+		var dropNumber = element.target.id.substr(idLength - 1, 1);
 	}
 	else if (idLength == 9) {
-		var dropNumber = element.id.substr(idLength - 1, 2);
+		var dropNumber = element.target.id.substr(idLength - 1, 2);
 	}
 
 
@@ -147,7 +188,19 @@ function onMessage(event) {
 
 	for (var i = 0; i < keys.length; i++) {
 		var key = keys[i];
+		//console.log("key: "+key);
+		// console.log("jsonMsg[key]: "+jsonMsg[key]);
+		/*if (key.includes("Dtoggle5")) {
+			if (jsonMsg[key] == "1") {
+				document.getElementById(key).checked = true;
 
+			}
+			if (jsonMsg[key] == "0") {
+				document.getElementById(key).checked = false;
+			}
+			darkmode(key);
+		}
+		else*/
 		if ((key.includes("toggle")) && (!key.includes("Dtoggle5"))) {
 			if (jsonMsg[key] == "1") {
 				document.getElementById(key).checked = true;
@@ -165,9 +218,10 @@ function onMessage(event) {
 		}
 		else if (key.includes("winddir")) {
 			var Data = jsonMsg[key];
-			var dirdata = Data - 180;
+			var dirdata = Data;
 			document.getElementById(key).innerHTML = Data;
-			
+			//document.getElementById("winddirarrow").innerHTML = '<div class="arrow-wrapper" style="transform: translateX(-50%) rotate(' + dirdata + 'deg);"><img  alt="img" src="Wind-Marker.svg"></div>';
+
 			const winDirArrow = document.getElementById("arrow-wrapper");
 
 			winDirArrow.style.transform = 'translateX(-50%) rotate(' + dirdata + 'deg)';
